5fe6e82737291e852b664e094ffc14698e7afb5d
Kyle Haynes <kyle.alexander.haynes@gmail.com>
2015-11-10 15:15:48 -0500

    retrieving value from pi server and displaying in app

--- app/src/main/java/com/example/kyle/raptorproject/Thermostat.java
+++ app/src/main/java/com/example/kyle/raptorproject/Thermostat.java
@@ -1,11 +1,23 @@
 package com.example.kyle.raptorproject;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ResponseCache;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class Thermostat extends AppCompatActivity {
 
@@ -15,6 +27,35 @@ public class Thermostat extends AppCompatActivity {
         setContentView(R.layout.activity_thermostat);
         SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar);
         final TextView seekBarValue = (TextView)findViewById(R.id.tempSetting);
+
+        Button onButton = (Button)findViewById(R.id.thermostatOnButton);
+        onButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button button=(Button) v;
+                String currentText = button.getText().toString();
+                switch(currentText) {
+                    case "On":
+                        SocketTask task = new SocketTask();
+                        String responseStr = null;
+                        try {
+                            responseStr = (String) task.execute().get();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } catch (ExecutionException e) {
+                            e.printStackTrace();
+                        }
+                        seekBarValue.setText(responseStr);
+                        button.setText("Off");
+                        break;
+                    case "Off":
+                        button.setText("On");
+                        break;
+                    default:
+                        button.setText("Error");
+                }
+            }
+        });
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
@@ -61,5 +102,55 @@ public class Thermostat extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class SocketTask extends AsyncTask {
+
+        @Override
+        protected String doInBackground(Object[] params) {
+            Log.d("Here:", "Task happened");
+            HttpURLConnection urlConnection = null;
+
+            try {
+                URL url = new URL("http://192.168.0.166:8080");
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                BufferedReader rd = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                String line;
+                StringBuffer response = new StringBuffer();
+                while ((line = rd.readLine()) != null) {
+                    response.append(line);
+                    response.append('\r');
+                }
+                rd.close();
+                String responseStr = response.toString();
+                TextView seekBarValue = (TextView)findViewById(R.id.tempSetting);
+
+                Log.d("Server Response", responseStr);
+                return responseStr;
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                urlConnection.disconnect();
+            }
+
+
+//        try {
+//            Socket socket = new Socket("192.168.0.166", 8080);
+//            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
+//            byte[] buffer = new byte[1024];
+//            int bytesRead;
+//            InputStream inputStream = socket.getInputStream();
+//            while ((bytesRead = inputStream.read(buffer)) != -1){
+//                byteArrayOutputStream.write(buffer,0,bytesRead);
+//                response += byteArrayOutputStream.toString("UTF-8");
+//            }
+//            Log.d("Here:", "We got something " + response);
+//        } catch (Exception e) {
+//            Log.d("Here:", "failed to create socket " + e.toString());
+//        }
+            return null;
+        }
+
     }
 }
--- app/src/main/java/com/example/kyle/raptorproject/ThermostatFragment.java
+++ app/src/main/java/com/example/kyle/raptorproject/ThermostatFragment.java
@@ -1,5 +1,6 @@
 package com.example.kyle.raptorproject;
 
+import android.app.DownloadManager;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -8,11 +9,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.Socket;
+import java.net.URL;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -24,55 +31,80 @@ public class ThermostatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
-
+//
+//
         View content = inflater.inflate(R.layout.fragment_thermostat, container, false);
-        Button onButton = (Button) content.findViewById(R.id.thermostatOnButton);
-        onButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Button button=(Button) v;
-                String currentText = button.getText().toString();
-                switch(currentText) {
-                    case "On":
-                        new SocketTask().execute();
-                        button.setText("Off");
-                        break;
-                    case "Off":
-                        button.setText("On");
-                        break;
-                    default:
-                        button.setText("Error");
-                }
-            }
-        });
+//        Button onButton = (Button) content.findViewById(R.id.thermostatOnButton);
+//        onButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Button button=(Button) v;
+//                String currentText = button.getText().toString();
+//                switch(currentText) {
+//                    case "On":
+//                        new SocketTask().execute();
+//                        button.setText("Off");
+//                        break;
+//                    case "Off":
+//                        button.setText("On");
+//                        break;
+//                    default:
+//                        button.setText("Error");
+//                }
+//            }
+//        });
         return content;
+//    }
+
+
+//private class SocketTask extends AsyncTask {
+//
+//    @Override
+//    protected Object doInBackground(Object[] params) {
+//        Log.d("Here:", "Task happened");
+//        HttpURLConnection urlConnection = null;
+//
+//        try {
+//            URL url = new URL("http://www.android.com/");
+//            urlConnection = (HttpURLConnection) url.openConnection();
+//
+//            BufferedReader rd = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+//            String line;
+//            StringBuffer response = new StringBuffer();
+//            while ((line = rd.readLine()) != null) {
+//                response.append(line);
+//                response.append('\r');
+//            }
+//            rd.close();
+//            String responseStr = response.toString();
+//            Log.d("Server Response", responseStr);
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally{
+//                urlConnection.disconnect();
+//            }
+//
+//
+//
+//
+////        try {
+////            Socket socket = new Socket("192.168.0.166", 8080);
+////            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
+////            byte[] buffer = new byte[1024];
+////            int bytesRead;
+////            InputStream inputStream = socket.getInputStream();
+////            while ((bytesRead = inputStream.read(buffer)) != -1){
+////                byteArrayOutputStream.write(buffer,0,bytesRead);
+////                response += byteArrayOutputStream.toString("UTF-8");
+////            }
+////            Log.d("Here:", "We got something " + response);
+////        } catch (Exception e) {
+////            Log.d("Here:", "failed to create socket " + e.toString());
+////        }
+////        return null;
+//    }
+//}
     }
-
-
-private class SocketTask extends AsyncTask {
-
-    @Override
-    protected Object doInBackground(Object[] params) {
-        Log.d("Here:", "Task happened");
-        String response = null;
-        try {
-            Socket socket = new Socket("192.168.1.119", 8000);
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            InputStream inputStream = socket.getInputStream();
-            while ((bytesRead = inputStream.read(buffer)) != -1){
-                byteArrayOutputStream.write(buffer,0,bytesRead);
-                response += byteArrayOutputStream.toString("UTF-8");
-            }
-            Log.d("Here:", "We got something " + response);
-        } catch (Exception e) {
-            Log.d("Here:", "failed to create socket " + e.toString());
-        }
-        return null;
-    }
-}
-
 }
 
